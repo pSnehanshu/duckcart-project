0443ee722497b34a2ed0e0a3a5c873bf5ad9901a
Snehanshu Phukon <hello@snehanshu.tech>
2020-05-17 09:50:55 +0530

    firsta card

--- src/components/main-area.vue
+++ src/components/main-area.vue
@@ -19,11 +19,34 @@
           <v-tab v-for="tab in info_tabs" :key="tab.name">{{ tab.label }}</v-tab>
         </v-tabs>
 
-        <v-tabs-items v-model="current_tab">
+        <v-tabs-items v-model="current_tab" class="mt-5">
           <v-tab-item>Empty</v-tab-item>
           <v-tab-item>
-            <v-card>
+            <v-card outlined shaped>
               <v-card-title>Information</v-card-title>
+              <v-simple-table>
+                <tbody>
+                  <tr>
+                    <th>EMAIl</th>
+                    <td>kamalpratap@xyz.com</td>
+                  </tr>
+                  <tr>
+                    <th>PHONE</th>
+                    <td>+919876543210</td>
+                  </tr>
+                  <tr>
+                    <th>SOCIAL</th>
+                    <td>github.com/kamalpratap</td>
+                  </tr>
+                  <tr>
+                    <th>LINKS</th>
+                    <td>
+                      <div>codepen.io/kamal</div>
+                      <div>yahoo.io/kamal</div>
+                    </td>
+                  </tr>
+                </tbody>
+              </v-simple-table>
             </v-card>
           </v-tab-item>
           <v-tab-item>Empty</v-tab-item>
